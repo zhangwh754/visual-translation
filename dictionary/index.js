73151edb18c3efdc8aeeeb2b50ed71996038131d
zhang <zhangwh754@163.com>
2024-11-11 19:28:14 +0800

    feat: 字典（部分）

---
+++ dictionary/index.js
@@ -0,0 +1,69 @@
+const SKILLS = [];
+
+const PHRASE = [
+  {
+    label: "You may choose from:",
+    value: "你可以从中选择：",
+  },
+];
+
+const ASCEND = [
+  {
+    label: "Form",
+    value: "型",
+  },
+  {
+    label: "Force",
+    value: "力",
+  },
+  {
+    label: "Life",
+    value: "命",
+  },
+  {
+    label: "Inertia",
+    value: "惯",
+  },
+  {
+    label: "Entropy",
+    value: "熵",
+  },
+  {
+    label: "Voracity",
+    value: "贪婪",
+  },
+  {
+    label: "Withered",
+    value: "枯萎",
+  },
+  {
+    label: "Defiance",
+    value: "蔑视",
+  },
+  {
+    label: "Purity",
+    value: "纯洁",
+  },
+  {
+    label: "Elementalist",
+    value: "元素主义者",
+  },
+  {
+    label: "Prosperity",
+    value: "繁荣",
+  },
+  {
+    label: "Prosperity",
+    value: "适应",
+  },
+  {
+    label: "Abeyance",
+    value: "搁置",
+  },
+  {
+    label: "Vitality Void",
+    value: "生命虚空",
+  },
+];
+
+const DICTIONARY = [...SKILLS, ...PHRASE, ...ASCEND];
